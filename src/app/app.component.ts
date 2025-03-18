9987261fbe6cd4c2ffb16271dafd6edf2adebd01
stanisev <shadowrageinc@gmail.com>
2025-03-18 21:11:36 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {AfterViewInit, Component} from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'stanisev-portfolio';
+export class AppComponent implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    gsap.to('.avatar', {
+      rotationY: 20,
+      repeat: -1,
+      yoyo: true,
+      duration: 1,
+      ease: 'power1.inOut',
+      transformOrigin: 'center center',
+    });
+
+    gsap.to('.gear_1', {
+      rotation: 360,
+      repeat: -1,
+      duration: 30,
+      ease: 'linear',
+      transformOrigin: 'center center',
+    });
+
+    gsap.to('.gear_2', {
+      rotation: -360,
+      repeat: -1,
+      duration: 15,
+      ease: 'linear',
+      transformOrigin: 'center center',
+    });
+
+    gsap.to('.gear_3', {
+      rotation: -360,
+      repeat: -1,
+      duration: 15,
+      ease: 'linear',
+      transformOrigin: 'center center',
+    });
+  }
+
 }
