d2fd3db9bf97d5676613d8f363ba052f041f3d40
stanisev <shadowrageinc@gmail.com>
2025-03-21 01:16:14 +0200

    redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  sections: NodeListOf<HTMLElement>;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) {
+    this.sections = this.el.nativeElement.querySelectorAll('section[id]');
+  }
 
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollUpElement = this.el.nativeElement.querySelector('#scroll-up');
+    if (window.scrollY >= 350) {
+      this.renderer.addClass(scrollUpElement, 'show-scroll');
+    } else {
+      this.renderer.removeClass(scrollUpElement, 'show-scroll');
+    }
+
+    const scrollDown = window.scrollY;
+
+    this.sections = this.el.nativeElement.querySelectorAll('section[id]');
+    this.sections.forEach((current) => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 58;
+      const sectionId = current.getAttribute('id');
+      const sectionLink = this.el.nativeElement.querySelector(`.nav__list a[href*="${sectionId}"]`);
+
+      if (sectionLink) {
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+          this.renderer.addClass(sectionLink, 'active-link');
+        } else {
+          this.renderer.removeClass(sectionLink, 'active-link');
+        }
+      }
+    });
+  }
 }
