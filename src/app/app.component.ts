4968721a542401519299dc5b79ade1a8bfc894fe
stanisev <shadowrageinc@gmail.com>
2025-03-20 21:56:22 +0200

    redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
-import { gsap } from 'gsap';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,43 +6,8 @@ import { gsap } from 'gsap';
   standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-    gsap.to('.avatar', {
-      rotationY: 20,
-      repeat: -1,
-      yoyo: true,
-      duration: 1,
-      ease: 'power1.inOut',
-      transformOrigin: 'center center',
-    });
-
-    gsap.to('.gear_1', {
-      rotation: 360,
-      repeat: -1,
-      duration: 30,
-      ease: 'linear',
-      transformOrigin: 'center center',
-    });
-
-    gsap.to('.gear_2', {
-      rotation: -360,
-      repeat: -1,
-      duration: 15,
-      ease: 'linear',
-      transformOrigin: 'center center',
-    });
-
-    gsap.to('.gear_3', {
-      rotation: -360,
-      repeat: -1,
-      duration: 15,
-      ease: 'linear',
-      transformOrigin: 'center center',
-    });
-  }
 
 }
